688b35b666db2a171ab57503e984bb44e8d3a9cf
xiexinfang <1036554353@qq.com>
2019-08-19 18:08:43 +0800

    practice-4

--- practice/section-1/practice-4.js
+++ practice/section-1/practice-4.js
@@ -5,7 +5,7 @@ function collectSameElements(collectionA, objectB) {
   let collectionB=objectB.value;
   for(var i=0;i<collectionA.length;i++){
     for(var j=0;j<collectionB.length;j++){
-      if(collectionA[i]['key']==collectionB[j]){
+      if(collectionA[i].key==collectionB[j]){
         result.push(collectionB[j]);
       }
     }
